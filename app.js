1676b97fa2ed954cd01d7dfeb76e9b7e3089db63
emoreta <emoreta202@outlook.com>
2025-02-17 22:08:49 -0500

    chnages in weigth files

--- app.js
+++ app.js
@@ -13,7 +13,9 @@ const PORT = process.env.PORT || 3500;
 const url = process.env.URL
 
 const app = express();
-app.use(express.json()); // Middleware para parsear JSON
+app.use(express.json({ limit: '50mb' })); 
+app.use(express.urlencoded({ extended: true, limit: '50mb' })); 
+
 
 app.use(cors({
     origin: '*'
